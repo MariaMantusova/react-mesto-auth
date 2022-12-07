118d864cf1836cd47bc933674915731e4eb68c73
MariaMantusova <maria.mantusova@mail.ru>
2022-12-07 13:00:04 +0300

    feat: add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import logoImage from "../images/header-logo.svg";
 
-function Header() {
+function Header(props) {
     return (
         <header className="header">
             <a href="#" className="header__link">
                 <img src={logoImage} alt="Логотип сайта" className="header__logo"/>
+            </a>
+            <a href="#" className="header__link">
+                <h2 className="header__registration-link">{props.link}</h2>
             </a>
         </header>
     )
