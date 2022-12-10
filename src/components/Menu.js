e5bfe71a73dc194cb859aef0231aa2d045f9b67c
MariaMantusova <maria.mantusova@mail.ru>
2022-12-10 16:29:09 +0300

    feat: add working hamburger

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,17 +3,19 @@ import {Link} from "react-router-dom";
 import {BiMenu} from "react-icons/bi"
 
 function Menu() {
+    const [open, setOpen] = React.useState(false);
+
     return (
         <>
-            <BiMenu className="burger"/>
-            <ul className="burger-links">
+            <BiMenu onClick={() => {setOpen(!open)}} className="burger"/>
+            {open && <ul className="burger-links">
                 <li className="burger__email">email</li>
                 <li className="burger__link">
                     <Link className="burger__link">
                         Выйти
                     </Link>
                 </li>
-            </ul>
+            </ul>}
         </>
     )
 }
