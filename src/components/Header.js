d349daf41f8d9af0bcde2831c7fd907146d290d1
MariaMantusova <maria.mantusova@mail.ru>
2022-12-10 16:36:26 +0300

    feat: change place of burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
 import logoImage from "../images/header-logo.svg";
 import {Link} from "react-router-dom";
 import Menu from "./Menu";
+import {BiMenu} from "react-icons/bi"
+import React from "react";
 
 function Header(props) {
+    const [open, setOpen] = React.useState(false);
+
     return (
-        <header className="header">
-            <a href="#" className="header__link">
-                <img src={logoImage} alt="Логотип сайта" className="header__logo"/>
-            </a>
-            <div className="header__links-container">
-                {props.children}
+        <>
+            <Menu opened={open} />
+            <header className="header">
                 <a href="#" className="header__link">
-                    <h2 className={`header__registration-link ${props.nameClass}`}>{`${props.link}`}</h2>
+                    <img src={logoImage} alt="Логотип сайта" className="header__logo"/>
                 </a>
-            </div>
-            <Menu />
-        </header>
+                <div className="header__links-container">
+                    {props.children}
+                    <a href="#" className="header__link">
+                        <h2 className={`header__registration-link ${props.nameClass}`}>{`${props.link}`}</h2>
+                    </a>
+                </div>
+                <BiMenu onClick={() => {
+                    setOpen(!open)
+                }} className="burger"/>
+            </header>
+        </>
     )
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {BiMenu} from "react-icons/bi"
 
-function Menu() {
-    const [open, setOpen] = React.useState(false);
-
+function Menu(props) {
     return (
         <>
-            <BiMenu onClick={() => {setOpen(!open)}} className="burger"/>
-            {open && <ul className="burger-links">
+            {props.opened && <ul className="burger-links">
                 <li className="burger__email">email</li>
                 <li className="burger__link">
                     <Link className="burger__link">
