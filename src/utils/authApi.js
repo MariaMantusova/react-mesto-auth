d47026340da5ac60ecdcad38c4120468d93f794b
MariaMantusova <maria.mantusova@mail.ru>
2022-12-12 13:55:50 +0300

    feat: add auth fetches

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -1,7 +1,62 @@
 class AuthApi {
     constructor(config) {
         this._url = config.url;
-        this._headers = config.headers;
+        this._header = config.headers;
+    }
+
+    registerUser(password, email) {
+        return fetch(`${this._url}/signup`, {
+            method: 'POST',
+            headers: this._header,
+            body: JSON.stringify({
+                password: password,
+                email: email
+            })
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(new Error(res.status.toString()));
+                }
+            })
+            .catch((err) => Promise.reject(err));
+    }
+
+    authorizeUser(password, email) {
+        return fetch(`${this._url}/signin`, {
+            method: 'POST',
+            headers: this._header,
+            body: JSON.stringify({
+                password: password,
+                email: email
+            })
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(new Error(res.status.toString()));
+                }
+            })
+            .catch((err) => Promise.reject(err));
+    }
+
+    validityCheck(JWT) {
+        return fetch(`${this._url}/users/me`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JWT}`
+            }
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(new Error(res.status.toString()));
+                }
+            })
+            .catch((err) => Promise.reject(err));
     }
 }
 
